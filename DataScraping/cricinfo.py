7f6709d137979d383dd73a88ed3c7e622f203446
sumandeepb <sumandeep.banerjee@gmail.com>
2019-05-24 09:52:56 +0530

    Fixed DataScraping and t-SNE Tut

--- DataScraping/cricinfo.py
+++ DataScraping/cricinfo.py
@@ -1,21 +1,15 @@
 import scrapy
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
-from scrapy.selector import Selector
-import re
 import json
-from scrapy.http import Request, FormRequest
-from collections import namedtuple
 import re
+from collections import namedtuple
 
 class CricInfoScrapeSpider(scrapy.Spider):
     name='cricinfo-spider'
     start_urls=['http://www.espncricinfo.com/ci/content/player/country.html?country=6;alpha=A']
     first_class_batting_stats=namedtuple("first_batting_stats", 'matches, inns, not_outs, runs, highest, average, balls_faced, strike_rate, hundreds, fifties, boundaries, sixes, catches_taken, stumpings_made')
-    #self.numPages=10
 
     def parse(self, response):
-        sel = Selector(text=response.body_as_unicode(), type="html")
+        sel = scrapy.Selector(text=response.body_as_unicode(), type="html")
         players_urlpath=sel.xpath(
             '//div[@id="ciPlayerbyCharAtoZ"]//ul//li//a/@href'
         )
@@ -25,19 +19,19 @@ class CricInfoScrapeSpider(scrapy.Spider):
             yield request
 
     def parse_player_names(self,response):
-        sel= Selector(text=response.body_as_unicode(), type="html")
+        sel= scrapy.Selector(text=response.body_as_unicode(), type="html")
         urlPath=sel.xpath(
             '//td[@class="ciPlayernames"]//a/@href'
         )
 
-        for url in urlPath.extract()[1:20]:
+        for url in urlPath.extract()[1:2]:
             url = "http://www.espncricinfo.com" + url
             request = scrapy.Request(url, self.parse_player_details)
             request.meta['url']=url
             yield request
 
     def parse_player_details(self, response):
-        sel = Selector(text=response.body_as_unicode(), type="html")
+        sel = scrapy.Selector(text=response.body_as_unicode(), type="html")
         name = sel.xpath(
             '//div[@class="ciPlayernametxt"]/div/h1/text()'
         )
